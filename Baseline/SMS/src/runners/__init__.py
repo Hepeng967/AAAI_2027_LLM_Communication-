ee8974a17a6b90f248eb0bc929f6fa1486159df4
Hepeng967 <Hepeng967@users.noreply.github.com>
2026-07-20 11:55:30 +0800

    Update SMS and MAIC baselines

--- Baseline/SMS/src/runners/__init__.py
+++ Baseline/SMS/src/runners/__init__.py
@@ -1,4 +1,6 @@
 REGISTRY = {}
 
 from .parallel_runner import ParallelRunner
+from .episode_runner import EpisodeRunner
 REGISTRY["parallel"] = ParallelRunner
+REGISTRY["episode"] = EpisodeRunner
---
+++ Baseline/SMS/src/runners/episode_runner.py
@@ -0,0 +1,145 @@
+from functools import partial
+
+import numpy as np
+
+from components.episode_buffer import EpisodeBatch
+from envs import REGISTRY as env_REGISTRY
+
+
+class EpisodeRunner:
+    def __init__(self, args, logger):
+        self.args = args
+        self.logger = logger
+        self.batch_size = self.args.batch_size_run
+        assert self.batch_size == 1
+
+        self.env = env_REGISTRY[self.args.env](**self.args.env_args)
+        self.episode_limit = self.env.episode_limit
+        self.t = 0
+        self.t_env = 0
+        self.train_returns = []
+        self.test_returns = []
+        self.train_stats = {}
+        self.test_stats = {}
+        self.log_train_stats_t = -1000000
+
+    def setup(self, scheme, groups, preprocess, mac):
+        self.new_batch = partial(
+            EpisodeBatch,
+            scheme,
+            groups,
+            self.batch_size,
+            self.episode_limit + 1,
+            preprocess=preprocess,
+            device=self.args.device,
+        )
+        self.mac = mac
+
+    def get_env_info(self):
+        return self.env.get_env_info()
+
+    def save_replay(self):
+        self.env.save_replay()
+
+    def close_env(self):
+        self.env.close()
+
+    def reset(self):
+        self.batch = self.new_batch()
+        self.env.reset()
+        self.t = 0
+
+    def run(self, test_mode=False):
+        self.reset()
+        terminated = False
+        episode_return = 0
+        msg_count = 0.0
+        self.mac.init_hidden(batch_size=self.batch_size)
+
+        while not terminated:
+            pre_transition_data = {
+                "state": [self.env.get_state()],
+                "avail_actions": [self.env.get_avail_actions()],
+                "obs": [self.env.get_obs()],
+            }
+            self.batch.update(pre_transition_data, ts=self.t)
+
+            actions = self.mac.select_actions(
+                self.batch, t_ep=self.t, t_env=self.t_env, test_mode=test_mode
+            )
+            reward, terminated, env_info = self.env.step(actions[0])
+            episode_return += reward
+            if self.args.mac == "sms_mac":
+                msg_count += self.mac.msg_count[0].sum().item()
+
+            post_transition_data = {
+                "actions": actions,
+                "reward": [(reward,)],
+                "terminated": [
+                    (terminated != env_info.get("episode_limit", False),)
+                ],
+            }
+            self.batch.update(post_transition_data, ts=self.t)
+            self.t += 1
+
+        last_data = {
+            "state": [self.env.get_state()],
+            "avail_actions": [self.env.get_avail_actions()],
+            "obs": [self.env.get_obs()],
+        }
+        self.batch.update(last_data, ts=self.t)
+        actions = self.mac.select_actions(
+            self.batch, t_ep=self.t, t_env=self.t_env, test_mode=test_mode
+        )
+        self.batch.update({"actions": actions}, ts=self.t)
+
+        cur_stats = self.test_stats if test_mode else self.train_stats
+        cur_returns = self.test_returns if test_mode else self.train_returns
+        log_prefix = "test_" if test_mode else ""
+        cur_stats.update(
+            {
+                key: cur_stats.get(key, 0) + env_info.get(key, 0)
+                for key in set(cur_stats) | set(env_info)
+            }
+        )
+        cur_stats["n_episodes"] = 1 + cur_stats.get("n_episodes", 0)
+        cur_stats["ep_length"] = self.t + cur_stats.get("ep_length", 0)
+        if self.args.mac == "sms_mac" and self.t > 0:
+            receiver_count = (
+                self.args.n_agents
+                if self.args.env == "listener_speaker"
+                else self.args.n_agents - 1
+            )
+            communication_rate = (
+                msg_count / self.args.n_agents / receiver_count / self.t
+            )
+            cur_stats["communication_rate"] = communication_rate + cur_stats.get(
+                "communication_rate", 0
+            )
+
+        if not test_mode:
+            self.t_env += self.t
+        cur_returns.append(episode_return)
+
+        if test_mode and len(self.test_returns) == self.args.test_nepisode:
+            self._log(cur_returns, cur_stats, log_prefix)
+        elif self.t_env - self.log_train_stats_t >= self.args.runner_log_interval:
+            self._log(cur_returns, cur_stats, log_prefix)
+            if hasattr(self.mac.action_selector, "epsilon"):
+                self.logger.log_stat(
+                    "epsilon", self.mac.action_selector.epsilon, self.t_env
+                )
+            self.log_train_stats_t = self.t_env
+
+        return self.batch
+
+    def _log(self, returns, stats, prefix):
+        self.logger.log_stat(prefix + "return_mean", np.mean(returns), self.t_env)
+        self.logger.log_stat(prefix + "return_std", np.std(returns), self.t_env)
+        returns.clear()
+        for key, value in stats.items():
+            if key != "n_episodes":
+                self.logger.log_stat(
+                    prefix + key + "_mean", value / stats["n_episodes"], self.t_env
+                )
+        stats.clear()
